39b1d5f665a458e1d735bd21c8cb36b3a8e0d6fa
Gourav Manjhi <gouravmanjhi9313@gmail.com>
2026-03-13 12:42:29 +0530

    Time: 6 ms (27.06%), Space: 43.4 MB (81.05%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        int h=0;
+        for(int i = n-1;i>=0;i--){
+            int papers = n-i;
+            if(citations[i]>=papers){
+                h = papers;
+            }
+        }
+        return h;
+    }
+}
