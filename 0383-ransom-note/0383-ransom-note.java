c4d4224f997892a0123a118e46970fbeaf37f7a2
Gourav Manjhi <gouravmanjhi9313@gmail.com>
2026-03-31 11:34:47 +0530

    Time: 1 ms (99.93%), Space: 46.1 MB (96.58%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] count = new int[26];
-
-        for(int i=0;i<magazine.length();i++){
-            count[magazine.charAt(i)-'a']++;
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
         }
-
-        for(int i=0;i<ransomNote.length();i++){
+        for (int i = 0; i < ransomNote.length(); i++) {
             char c = ransomNote.charAt(i);
-
-            if(count[c-'a']==0){
+            if (count[c - 'a'] == 0) {
                 return false;
             }
-            count[c-'a']--;
+            count[c - 'a']--;
         }
         return true;
     }
