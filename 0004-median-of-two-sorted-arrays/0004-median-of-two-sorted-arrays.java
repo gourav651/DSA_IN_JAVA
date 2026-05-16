f03265e3c465e31c1fa6ddcfa712aaa5214d5997
Gourav Manjhi <gouravmanjhi9313@gmail.com>
2026-05-16 12:26:09 +0530

    Time: 1 ms (100%), Space: 48.7 MB (85.3%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,26 +3,28 @@ class Solution {
         int n1 = nums1.length;
         int n2 = nums2.length;
 
-        int arr[] = new int[n1+n2];
-        int i=0,j=0,k=0;
+        int arr[] = new int[n1 + n2];
+        int i = 0, j = 0, k = 0;
 
-        while(i<n1 && j<n2){
-            if(nums1[i] < nums2[j]){
+        //1. FIRST STEP : PUSH ALL THE ELEMENTS OF BOTH ARRAYS INTO THE NEW ARRAY arr[] IN SORTED ORDER
+
+        while (i < n1 && j < n2) {
+            if (nums1[i] < nums2[j]) {
                 arr[k] = nums1[i];
                 i++;
                 k++;
-            }else{
+            } else {
                 arr[k] = nums2[j];
                 j++;
                 k++;
             }
         }
-        while(i < n1){
+        while (i < n1) {
             arr[k] = nums1[i];
             i++;
             k++;
         }
-        while(j<n2){
+        while (j < n2) {
             arr[k] = nums2[j];
             j++;
             k++;
@@ -30,10 +32,12 @@ class Solution {
 
         int n = arr.length;
 
-        if(n%2==1){
-            return arr[n/2];
+        //2.SECOND STEP : CHECK THE SIZE OF NEW ARRAY, IF EVEN OR ODD THEN BELOW CODE
+
+        if (n % 2 == 1) { // IF ODD LENGTH(because remainder 1)
+            return arr[n / 2];
         }
-        
-        return (arr[n/2] + arr[n/2-1])/2.0;
+
+        return (arr[n / 2] + arr[n / 2 - 1]) / 2.0; // IF EVEN LENGTH(becasue in even legnth wo numbers are att middle, so take the avg of both)
     }
 }
