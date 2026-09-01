3c73c48d72594dd65fb94b4e9b5056f19d628e24
Gourav Manjhi <gouravmanjhi9313@gmail.com>
2026-09-01 23:17:18 +0530

    Time: 2 ms (64.23%), Space: 46.3 MB (65.17%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,29 +13,29 @@
  *     }
  * }
  */
-class Solution{
-    int preIndex=0;
-    HashMap<Integer, Integer> map = new HashMap<>();
+class Solution {
+    int preIndex = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();    //to store the inorder array elements
 
-    public TreeNode buildTree(int[] preorder, int[] inorder){
-        for(int i=0;i< inorder.length;i++){
-            map.put(inorder[i],i);
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);     //store the inorder arr elements into the map
         }
-        return build(preorder, 0, inorder.length-1);
+        return build(preorder, 0, inorder.length - 1);
     }
 
-    private TreeNode build(int[] preorder, int left, int right){
-        if(left > right){
+    private TreeNode build(int[] preorder, int left, int right) {
+        if (left > right) {     //if the child not build i.e., left > right
             return null;
         }
 
-        int rootValue = preorder[preIndex++];
-        TreeNode root = new TreeNode(rootValue);
+        int rootValue = preorder[preIndex++]; //find the root value
+        TreeNode root = new TreeNode(rootValue); //store the root value in root
 
-        int index = map.get(rootValue);
+        int index = map.get(rootValue); // store the root value index
 
-        root.left = build(preorder, left, index-1);
-        root.right = build(preorder, index+1, right);
+        root.left = build(preorder, left, index - 1); //recursive process to do the same process for the left child
+        root.right = build(preorder, index + 1, right); // recursive process to do the same process for the right child
         return root;
     }
 }
